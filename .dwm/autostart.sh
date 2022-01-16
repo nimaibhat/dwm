a5f604c41511fef73409f27c01b1e7d568072d25
nbhat0 <92162938+nbhat0@users.noreply.github.com>
2022-01-16 11:28:52 -0500

    Update autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
-picom --experimental-backends -b &
-xwallpaper --zoom ~/.config/wall.png &
-dwmblocks & 
-ckb-next --background &
-sudo ntpd -qg &
+# These are just some of my personal background things, you can put whatever
+picom --experimental-backends -b & # For picom-ibhagwan-git
+xwallpaper --zoom ~/.config/wall.png & # For setting wallpaper, need to have xwallpaper installed through pacman
+dwmblocks & # If using dwmblocks keep this line
+ckb-next --background & 
+sudo ntpd -qg & # To sync time (I have some clock drift not sure why)
