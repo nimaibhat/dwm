831097dbeaea46b6bd4a680a81993d6789fa09cc
nbhat0 <92162938+nbhat0@users.noreply.github.com>
2022-01-24 17:17:23 -0500

    Add files via upload

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# These are just some of my personal background things, you can put whatever
+
 picom --experimental-backends -b & # For picom-ibhagwan-git
 xwallpaper --zoom ~/.config/wall.png & # For setting wallpaper, need to have xwallpaper installed through pacman
 dwmblocks & # If using dwmblocks keep this line
-ckb-next --background & 
+/usr/bin/syncthing serve --no-browser --logfile=default &
+ckb-next --background &
 sudo ntpd -qg & # To sync time (I have some clock drift not sure why)
